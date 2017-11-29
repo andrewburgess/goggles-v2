24b45f2fe3ecd66a847d67d80a5ef23f39c0185e
Andrew Burgess <andrew@andrewburgess.io>
2017-11-28 20:01:57 -0800

    :books: docs: update hardware and software used

--- goggles.ino
+++ goggles.ino
@@ -5,15 +5,15 @@ GOGGLES V2
 A new version of lightup goggles, using an LED strip and two 8x8 LED matrices
 
 Hardware:
-    - FLORA microcontroller (https://www.adafruit.com/product/659)
+    - Feather M0 Basic Proto (https://www.adafruit.com/product/2772)
     - Electret Microphone Amplifier (https://www.adafruit.com/product/1713)
     - DotStar LED Strip - 60 LED / meter (https://www.adafruit.com/product/2239)
     - 2x DotStar 8x8 Grid (https://www.adafruit.com/product/3444)
 
 Software Libraries:
     - Adafruit_DotStar (https://github.com/adafruit/Adafruit_DotStar)
-    - Adafruit_DotStarMatrix (https://github.com/adafruit/Adafruit_DotStarMatrix)
     - Adafruit-GFX-Library (https://github.com/adafruit/Adafruit-GFX-Library)
+    - QList (https://github.com/SloCompTech/QList)
 
 ******************************************************************************/
 
